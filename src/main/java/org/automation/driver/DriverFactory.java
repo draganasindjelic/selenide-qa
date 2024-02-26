f5e12bcf7de0a55d9066a8db36bed2d3ec96c020
Dragana Sindjelic <60484384+draganasindjelic@users.noreply.github.com>
2024-02-26 01:22:11 +0100

    WebDriver setup added (#2)

---
+++ src/main/java/org/automation/driver/DriverFactory.java
@@ -0,0 +1,30 @@
+package org.automation.driver;
+
+import org.automation.configuration.factory.ConfigFactory;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static void createDriver() {
+        if (ConfigFactory.getConfig().runMode().equals("remote")) {
+            //remote WebDriver setup
+        } else {
+            createLocalBrowser(ConfigFactory.getConfig().browser());
+        }
+    }
+
+    private static void createLocalBrowser(String browser) {
+        if (browser.equals("chrome")) {
+            ChromeDriver driver = new ChromeDriver();
+            DriverManager.driver.set(driver);
+            DriverManager.driver.get().manage().window().maximize();
+            DriverManager.driver.get().manage().deleteAllCookies();
+        } else if (browser.equals("firefox")) {
+            FirefoxDriver driver = new FirefoxDriver();
+            DriverManager.driver.set(driver);
+            DriverManager.driver.get().manage().window().maximize();
+            DriverManager.driver.get().manage().deleteAllCookies();
+        }
+    }
+}
